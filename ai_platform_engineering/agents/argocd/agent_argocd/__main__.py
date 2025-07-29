beabf3bc3d91c921ce4c8500522681b4c15d1265
Sri Aradhyula <sraradhy@cisco.com>
2025-07-29 10:48:36 -0500

    wip

--- ai_platform_engineering/agents/argocd/agent_argocd/__main__.py
+++ ai_platform_engineering/agents/argocd/agent_argocd/__main__.py
@@ -10,7 +10,11 @@ from agent_argocd.protocol_bindings.a2a_server.agent_executor import ArgoCDAgent
 
 from a2a.server.apps import A2AStarletteApplication
 from a2a.server.request_handlers import DefaultRequestHandler
-from a2a.server.tasks import InMemoryPushNotifier, InMemoryTaskStore
+from a2a.server.tasks import (
+    BasePushNotificationSender,
+    InMemoryPushNotificationConfigStore,
+    InMemoryTaskStore,
+)
 from a2a.types import (
     AgentCapabilities,
     AgentCard,
@@ -26,24 +30,30 @@ load_dotenv()
 @click.option('--host', 'host', default='localhost')
 @click.option('--port', 'port', default=10000)
 def main(host: str, port: int):
-    client = httpx.AsyncClient()
+    httpx_client = httpx.AsyncClient()
+
+    push_config_store = InMemoryPushNotificationConfigStore()
+    push_sender = BasePushNotificationSender(httpx_client=httpx_client,
+                    config_store=push_config_store)
     request_handler = DefaultRequestHandler(
-        agent_executor=ArgoCDAgentExecutor(),
-        task_store=InMemoryTaskStore(),
-        push_notifier=InMemoryPushNotifier(client),
+      agent_executor=ArgoCDAgentExecutor(),
+      task_store=InMemoryTaskStore(),
+      push_config_store=push_config_store,
+      push_sender= push_sender
     )
 
     server = A2AStarletteApplication(
-        agent_card=get_agent_card(host, port), http_handler=request_handler
+      agent_card=get_agent_card(host, port),
+      http_handler=request_handler
     )
     app = server.build()
 
     # Add CORSMiddleware to allow requests from any origin (disables CORS restrictions)
     app.add_middleware(
-          CORSMiddleware,
-          allow_origins=["*"],  # Allow all origins
-          allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
-          allow_headers=["*"],  # Allow all headers
+      CORSMiddleware,
+      allow_origins=["*"],  # Allow all origins
+      allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+      allow_headers=["*"],  # Allow all headers
     )
 
     import uvicorn
@@ -69,7 +79,7 @@ def get_agent_card(host: str, port: int):
     name='ArgoCD CRUD Agent',
     description='Agent for managing ArgoCD applications with CRUD operations.',
     url=f'http://{host}:{port}/',
-    version='1.0.0',
+    version='0.1.0',
     defaultInputModes=ArgoCDAgent.SUPPORTED_CONTENT_TYPES,
     defaultOutputModes=ArgoCDAgent.SUPPORTED_CONTENT_TYPES,
     capabilities=capabilities,
