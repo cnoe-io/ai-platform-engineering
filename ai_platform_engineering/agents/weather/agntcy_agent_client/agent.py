42e19eed97d814479ae0ffc37ae339c6f0914321
suwhang-cisco <suwhang@cisco.com>
2025-07-30 17:48:00 +0000

    chore: clean up react agents
    Signed-off-by: suwhang-cisco <suwhang@cisco.com>

--- ai_platform_engineering/agents/weather/agntcy_agent_client/agent.py
+++ ai_platform_engineering/agents/weather/agntcy_agent_client/agent.py
@@ -1,8 +1,6 @@
 # Copyright 2025 CNOE Contributors
 # SPDX-License-Identifier: Apache-2.0
 
-from langgraph.prebuilt import create_react_agent
-from cnoe_agent_utils import LLMFactory
 import os
 
 from ai_platform_engineering.agents.weather.agntcy_agent_client.agentcard import (
@@ -12,25 +10,13 @@ from ai_platform_engineering.agents.weather.agntcy_agent_client.agentcard import
 from ai_platform_engineering.utils.agntcy.agntcy_remote_agent_connect import (
     AgntcySlimRemoteAgentConnectTool,
 )
-from ai_platform_engineering.multi_agents.platform_engineer.prompts import get_agent_system_prompt
 
-model = LLMFactory().get_llm()
-
-WEATHER_ENDPOINT = os.getenv("WEATHER_AGENT_ENDPOINT", f"http://{weather_agent_card.url}")
+SLIM_ENDPOINT = os.getenv("SLIM_ENDPOINT", "http://slim-dataplane:46357")
 
 # initialize the flavor profile tool with the farm agent card
 weather_agntcy_remote_agent = AgntcySlimRemoteAgentConnectTool(
     name="weather_agntcy_remote_agent",
     description=WEATHER_AGENT_DESCRIPTION,
-    endpoint="http://slim-dataplane:46357",
+    endpoint=SLIM_ENDPOINT,
     remote_agent_card=weather_agent_card,
-)
-
-weather_system_prompt = get_agent_system_prompt("weather")
-
-weather_agent = create_react_agent(
-    model=model,
-    tools=[weather_agntcy_remote_agent],
-    name="weather_agent",
-    prompt=weather_system_prompt,
 )
