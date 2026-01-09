a0fae5c7412f3d873d39373aa89eac3a2d99a8d1
suwhang-cisco <suwhang@cisco.com>
2026-01-09 12:22:15 +0000

    feat: use -rc.X suffix to decide retag or all rebuilds
    Signed-off-by: suwhang-cisco <suwhang@cisco.com>

--- ai_platform_engineering/multi_agents/agent_registry.py
+++ ai_platform_engineering/multi_agents/agent_registry.py
@@ -15,6 +15,7 @@ across the platform, with integrated agent enablement logic.
 # Sunny V6 oh yeah NOPT
 # Sunny V7 oh yeah NOPT
 # Sunny V8 oh yeah NOT
+# Sunny V9 oh yeah NOT
 import os
 import logging
 import httpx
